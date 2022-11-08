f2ba4302db9535e0f20be7e4b934a8621a242c73
Sticko97 <sle23@my.bcit.ca>
2022-11-08 15:41:03 -0800

    array methods and functions

--- js/script.js
+++ js/script.js
@@ -13,15 +13,33 @@
 //     i++;
 // };
 
-let input;
-do{
-    let input = prompt( "what is your name?");
-    //prompt always returns string
-    //convert input to number
-    console.log(Number(input));
-    }
-    while(!isNaN(Number(input)));
+
+//                USING DO AND WHILE LOOPS TO GET AN INPUT FROM USER
+// let input;
+// do{
+//     let input = prompt( "what is your name?");
+//     //prompt always returns string
+//     //convert input to number
+//     console.log(Number(input));
+//     }
+//     while(!isNaN(Number(input)));
     
-alert("Hello " + input);
-alert('Hello ${input}');
-    //outputs user input as int
+// alert("Hello " + input);
+// alert('Hello ${input}');
+//outputs user input as int
+
+//               ADDING AND REMOVING FROM AN ARRAY  https://www.w3schools.com/jsref/jsref_obj_array.asp list of functions  
+
+let fruits=['Apple','Banana'];
+fruits.push("Orange");
+console.log(fruits);
+
+let popResult = fruits.pop();
+console.log(popResult);
+
+fruits.unshift("Mango");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
