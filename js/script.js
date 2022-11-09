ee661c53e2a965377cab6609683500cdc675af14
Sticko97 <sle23@my.bcit.ca>
2022-11-08 16:13:21 -0800

    activity for objects and comparison

--- js/script.js
+++ js/script.js
@@ -50,11 +50,46 @@
 // }
 
 //              OBJECTS IN JAVASCRIPT
-const person = {
-    name: ['Bob', 'Smith',],
-    age: 32,
-    gender: 'male',
-    interests: ['music', 'skiing']
+// const person = {
+//     name: ['Bob', 'Smith',],
+//     age: 32,
+//     gender: 'male',
+//     interests: ['music', 'skiing']
+// };
+
+// console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.interests[0]} and ${person.interests[1]};`)
+
+// for (let key in person)
+// {
+//     console.log(key);
+//     console.log(person[key]);
+// }
+
+//              FINDOUT OUT HOW TO COMPARE OBJECTS IN AN ARRAY
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
 };
 
-console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.interests[0]} and ${person.interests[1]};`)
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3];
+let counter = 0;
+for (let student in students)
+{
+    if (student.program === "CIT" && GPA > 3) {
+        counter++;
+        console.log(student);
+    }
+}
+console.log(counter);
