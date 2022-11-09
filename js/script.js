a480a9f0829f3fb693939b4e8b02c2a3341dd08d
Sticko97 <sle23@my.bcit.ca>
2022-11-08 16:00:12 -0800

    objects in javascript

--- js/script.js
+++ js/script.js
@@ -30,16 +30,31 @@
 
 //               ADDING AND REMOVING FROM AN ARRAY  https://www.w3schools.com/jsref/jsref_obj_array.asp list of functions  
 
-let fruits=['Apple','Banana'];
-fruits.push("Orange");
-console.log(fruits);
+// let fruits=['Apple','Banana'];
+// fruits.push("Orange");
+// console.log(fruits);
 
-let popResult = fruits.pop();
-console.log(popResult);
+// let popResult = fruits.pop();
+// console.log(popResult);
 
-fruits.unshift("Mango");
-console.log(fruits);
+// fruits.unshift("Mango");
+// console.log(fruits);
 
-fruits.shift();
-console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
 
+//              LOOPING IN AN ARRAY
+// let fruits=['Apple', 'Banana'];
+// for (let item of fruits){
+//     console.log(item);
+// }
+
+//              OBJECTS IN JAVASCRIPT
+const person = {
+    name: ['Bob', 'Smith',],
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing']
+};
+
+console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.interests[0]} and ${person.interests[1]};`)
